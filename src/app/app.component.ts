1c3c3b4b118a62aadf73e0d770b467d93d38f495
sourabh sen <sourabhchill@gmail.com>
2016-08-30 10:18:08 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:'my-app',
+  template:'<h1>Hi my app webpack !!!</h1>'    
+})
+export class AppComponent{
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var htmlwebplugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:'./src/main.ts',
+    output:{
+        path:'dist',
+        filename:'app.bundle.js'
+    },
+
+    module:{
+        loaders:[
+            {test: /\.ts$/, loader: 'ts'}
+        ]
+    },
+    resolve:{
+        extensions:['','.js','.ts']
+    },
+    plugins:[
+        new htmlwebplugin({
+            template:'./src/index.html'
+        })
+    ]
+
+}
