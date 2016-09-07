37fb5e2c7322b6547ca81625ec1536d0c05be84e
Sourabh Sen <Sourabh.Sen@corp.realpage.com>
2016-09-07 18:44:40 +0530

    changes for loader

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-
+import {Component,OnInit, ElementRef} from '@angular/core';
+import {window} from '@angular/platform-browser/src/facade/browser';
 import {ProfileComponent} from '../profile/profile.component';
 import {ProfileWidgetComponent} from '../profile-widget/profileWidget.component';
 import {ProfileAboutWidgetComponent} from '../profile-about-widget/profileAboutWidget.component';
@@ -10,6 +10,12 @@ import {ProfileAboutWidgetComponent} from '../profile-about-widget/profileAboutW
   directives: [ProfileWidgetComponent,ProfileAboutWidgetComponent]    
 })
 
-export class HomeComponent{
+export class HomeComponent implements OnInit{
+  constructor(private element:ElementRef){
+       
+  }
 
+  ngOnInit(){
+      window.$('body').addClass('grid-loaded');
+  }
 }
