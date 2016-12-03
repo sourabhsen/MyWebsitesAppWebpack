7a794f0a518059d7de607326d552b0ce0e4848c5
sourabh sen <sourabhchill@gmail.com>
2016-12-04 04:05:29 +0530

    hom epage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,140 +28,352 @@ export class HomeComponent implements OnInit{
   }
 
   ngOnInit(){
-     // temporary code for removing loader
-      window.$('body').addClass('grid-loaded');
+            // temporary code for removing loader
+            window.$('body').addClass('grid-loaded');
 
-      //Canvas header Code
-        var width, height, largeHeader, canvas, ctx, triangles, target, animateHeader = true;
-    var colors = ['220,220,220', '229,229,229', '153,153,153', '228,220,245', '153,158,150'];
+            //Canvas header Code
+                var width, height, largeHeader, canvas, ctx, triangles, target, animateHeader = true;
+                var colors = ['220,220,220', '229,229,229', '153,153,153', '228,220,245', '153,158,150'];
 
-    // Main
-    initHeader();
-    addListeners();
-    initAnimation();
+                // Main
+                initHeader();
+                addListeners();
+                initAnimation();
 
-    function initHeader() {
-        width = window.$('.header-wrapper').width() + 60;
-        height = window.$('.header-wrapper').height() + 70;
-        target = {x: 0, y: height};
+            function initHeader() {
+                width = window.$('.header-wrapper').width() + 60;
+                height = window.$('.header-wrapper').height() + 70;
+                target = {x: 0, y: height};
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height +'px';
+                largeHeader = document.getElementById('large-header');
+                largeHeader.style.height = height +'px';
 
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
+                canvas = document.getElementById('demo-canvas');
+                canvas.width = width;
+                canvas.height = height;
+                ctx = canvas.getContext('2d');
 
-        // create particles
-        triangles = [];
-        for(var x = 0; x < 480; x++) {
-            addTriangle(x*10);
-        }
-    }
-
-    function addTriangle(delay) {
-        setTimeout(function() {
-            var t = new Triangle();
-            triangles.push(t);
-            tweenTriangle(t);
-        }, delay);
-    }
-
-    function initAnimation() {
-        animate();
-    }
-
-    function tweenTriangle(tri) {
-        var t = Math.random()*(2*Math.PI);
-        var x = (600+Math.random()*100)*Math.cos(t) + width*0.5;
-        var y = (200+Math.random()*100)*Math.sin(t) + height*0.5-20;
-        var time = 4+3*Math.random();
-
-        window.TweenLite.to(tri.pos, time, {x: x,
-            y: y, ease:window.Circ.easeOut,
-            onComplete: function() {
-                tri.init();
-                tweenTriangle(tri);
-        }});
-    }
-
-    // Event handling
-    function addListeners() {
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.$('.header-wrapper').width() + 60;
-        height = window.$('.header-wrapper').height() + 70;;
-        largeHeader.style.height = height+'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in triangles) {
-                triangles[i].draw();
+                // create particles
+                triangles = [];
+                for(var x = 0; x < 480; x++) {
+                    addTriangle(x*10);
+                }
             }
-        }
-        requestAnimationFrame(animate);
-    }
 
-    // Canvas manipulation
-    function Triangle() {
-        var _this = this;
+            function addTriangle(delay) {
+                setTimeout(function() {
+                    var t = new Triangle();
+                    triangles.push(t);
+                    tweenTriangle(t);
+                }, delay);
+            }
 
-        // constructor
-        (function() {
-            _this.coords = [{},{},{}];
-            _this.pos = {};
-            init();
-        })();
+            function initAnimation() {
+                animate();
+            }
 
-        function init() {
-            _this.pos.x = width*0.5;
-            _this.pos.y = height*0.5-20;
-            _this.coords[0].x = -10+Math.random()*40;
-            _this.coords[0].y = -10+Math.random()*40;
-            _this.coords[1].x = -10+Math.random()*40;
-            _this.coords[1].y = -10+Math.random()*40;
-            _this.coords[2].x = -10+Math.random()*40;
-            _this.coords[2].y = -10+Math.random()*40;
-            _this.scale = 0.1+Math.random()*0.3;
-            _this.color = colors[Math.floor(Math.random()*colors.length)];
-            setTimeout(function() { _this.alpha = 0.8; }, 10);
-        }
+            function tweenTriangle(tri) {
+                var t = Math.random()*(2*Math.PI);
+                var x = (600+Math.random()*100)*Math.cos(t) + width*0.5;
+                var y = (200+Math.random()*100)*Math.sin(t) + height*0.5-20;
+                var time = 4+3*Math.random();
 
-        this.draw = function() {
-            if(_this.alpha >= 0.005) _this.alpha -= 0.005;
-            else _this.alpha = 0;
+                window.TweenLite.to(tri.pos, time, {x: x,
+                    y: y, ease:window.Circ.easeOut,
+                    onComplete: function() {
+                        tri.init();
+                        tweenTriangle(tri);
+                }});
+            }
 
-            
-            
-            ctx.beginPath();
-            ctx.moveTo(_this.coords[0].x+_this.pos.x, _this.coords[0].y+_this.pos.y);
-            ctx.lineTo(_this.coords[1].x+_this.pos.x, _this.coords[1].y+_this.pos.y);
-            ctx.lineTo(_this.coords[2].x+_this.pos.x, _this.coords[2].y+_this.pos.y);
-             
-            ctx.closePath();
-            ctx.fillStyle = 'rgba('+_this.color+','+ _this.alpha+')';
-          
-            ctx.fill();
+            // Event handling
+            function addListeners() {
+                window.addEventListener('scroll', scrollCheck);
+                window.addEventListener('resize', resize);
+            }
 
-            
+            function scrollCheck() {
+                if(document.body.scrollTop > height) animateHeader = false;
+                else animateHeader = true;
+            }
 
-        };
+            function resize() {
+                width = window.$('.header-wrapper').width() + 60;
+                height = window.$('.header-wrapper').height() + 70;;
+                largeHeader.style.height = height+'px';
+                canvas.width = width;
+                canvas.height = height;
+            }
 
-        this.init = init;
-    }
+            function animate() {
+                if(animateHeader) {
+                    ctx.clearRect(0,0,width,height);
+                    for(var i in triangles) {
+                        triangles[i].draw();
+                    }
+                }
+                requestAnimationFrame(animate);
+            }
+
+            // Canvas manipulation
+            function Triangle() {
+                var _this = this;
+
+                // constructor
+                (function() {
+                    _this.coords = [{},{},{}];
+                    _this.pos = {};
+                    init();
+                })();
+
+                function init() {
+                    _this.pos.x = width*0.5;
+                    _this.pos.y = height*0.5-20;
+                    _this.coords[0].x = -10+Math.random()*40;
+                    _this.coords[0].y = -10+Math.random()*40;
+                    _this.coords[1].x = -10+Math.random()*40;
+                    _this.coords[1].y = -10+Math.random()*40;
+                    _this.coords[2].x = -10+Math.random()*40;
+                    _this.coords[2].y = -10+Math.random()*40;
+                    _this.scale = 0.1+Math.random()*0.3;
+                    _this.color = colors[Math.floor(Math.random()*colors.length)];
+                    setTimeout(function() { _this.alpha = 0.8; }, 10);
+                }
+
+                this.draw = function() {
+                    if(_this.alpha >= 0.005) _this.alpha -= 0.005;
+                    else _this.alpha = 0;
+
+                    
+                    
+                    ctx.beginPath();
+                    ctx.moveTo(_this.coords[0].x+_this.pos.x, _this.coords[0].y+_this.pos.y);
+                    ctx.lineTo(_this.coords[1].x+_this.pos.x, _this.coords[1].y+_this.pos.y);
+                    ctx.lineTo(_this.coords[2].x+_this.pos.x, _this.coords[2].y+_this.pos.y);
+                    
+                    ctx.closePath();
+                    ctx.fillStyle = 'rgba('+_this.color+','+ _this.alpha+')';
+                
+                    ctx.fill();
+
+                    
+
+                };
+
+                this.init = init;
+            }
+    
+      	
+         
+        window.$(".UI").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                labelHeight: 30,
+                textFail: "Download Failed",
+				textComplete: Math.floor(Math.random() * 21) + 80 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+         window.$(".njs").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                labelHeight: 30,
+                textFail: "Download Failed",
+				textComplete: Math.floor(Math.random() * 21) + 80 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+         window.$(".angjs").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                labelHeight: 30,
+                textFail: "Download Failed",
+				textComplete: Math.floor(Math.random() * 21) + 80 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+        
+         window.$(".wbt").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                labelHeight: 30,
+                textFail: "Download Failed",
+				textComplete: Math.floor(Math.random() * 21) + 80 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+        
+         window.$(".nodejs").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                labelHeight: 30,
+                textFail: "Download Failed",
+				textComplete: Math.floor(Math.random() * 21) + 80 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+        
+         window.$(".java").ElasticProgress({
+				buttonSize: 60,
+				fontFamily: "Montserrat",
+				colorBg: "#dcdcdc",
+				colorFg: "#4ebcc4",
+                textFail: "Download Failed",
+                labelHeight: 30,
+				textComplete: Math.floor(Math.random() * 41) + 60 + '%',
+			
+				onOpen: function(event) {
+						console.log("onOpen");
+						fakeLoading(window.$(this),2,2);
+				},
+				onComplete: function(event) {
+						console.log("onComplete");
+				},
+				onClose: function(event) {
+						console.log("onClose");
+				},
+				onFail: function(event) {
+						console.log("onFail");
+						window.$(this).ElasticProgress("open");
+				},
+				onCancel: function(event) {
+						console.log("onCancel");
+						window.$(this).ElasticProgress("open");
+				}
+		});
+        
+        
+        window.$(".UI,.wbt,.angjs,.njs,.java,.nodejs").ElasticProgress("open");
+	
+		function fakeLoading($obj, speed, failAt) {
+				if (typeof speed == "undefined") speed = 2;
+				if (typeof failAt == "undefined") failAt = -1;
+				var v = 0;
+				var l = function() {
+						if (failAt > -1) {
+								if (v >= failAt) {
+										if (typeof $obj.jquery != "undefined") {
+												$obj.ElasticProgress("fail");
+										} else {
+												$obj.fail();
+										}
+										return;
+								}
+						}
+						v += Math.pow(Math.random(), 2) * 0.1 * speed;
+
+						if (typeof $obj.jquery != "undefined") {
+								$obj.ElasticProgress("setValue", v);
+						} else {
+								$obj.setValue(v);
+						}
+						if (v < 1) {
+								window.TweenMax.delayedCall(0.05 + (Math.random() * 0.14), l)
+						}
+				};
+				l();
+		}
+
+ 
+
+
+
+
 
 
       //End
